8205c3f9a184abebb6fd60f7ab23ab5cbb207e17
fadi123321 <fadibatraki321@gmail.com>
2026-08-27 15:11:35 +0300

    Restart Node apps automatically after code changes

--- roles/node_apps/files/server1.js
+++ roles/node_apps/files/server1.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Fadi from Node App 1");
+  res.send("Hello TONY from Node App 1");
 });
 
 app.listen(3001, "127.0.0.1", () => {
