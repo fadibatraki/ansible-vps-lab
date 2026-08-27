321a35c30942d02afcb3e70692fe9bc9ec7db372
fadi123321 <fadibatraki321@gmail.com>
2026-08-27 15:22:26 +0300

    Test automatic Node restart

--- roles/node_apps/files/server1.js
+++ roles/node_apps/files/server1.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello TONY from Node App 1");
+  res.send("Hello Rami from Node App 1");
 });
 
 app.listen(3001, "127.0.0.1", () => {
